15cbc375db51535980e49d5f90436a795973f184
Jqwop <jacobmbanghart@gmail.com>
2020-04-23 14:37:06 -0500

    Create hold.sh

---
+++ hold.sh
@@ -0,0 +1,4 @@
+apt-get install mysql-server mysql-client libmysqlclient-dev
+apt-get install apache2 apache2-doc apache2-npm-prefork apache2-utils libexpat1 ssl-cert
+apt-get install libapache2-mod-php7.0 php7.0 php7.0-common php7.0-curl php7.0-dev php7.0-gd php-pear php-imagick php7.0-mcrypt php7.0-mysql php7.0-ps php7.0-xsl
+apt-get install phpmyadmin
